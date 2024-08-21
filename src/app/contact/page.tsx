62d5714423805171fb875c02db3fc1185fcb84b8
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-08-21 14:27:30 -0400

    fix icons center

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -88,7 +88,7 @@ const ContactPage = () => {
             <ul className="flex flex-col gap-10">
               {info.map((item, index) => (
                 <li key={index} className="flex items-center gap-6">
-                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
                     <div className="text-[28px]">{item.icon}</div>
                   </div>
                   <div className="flex-1">
